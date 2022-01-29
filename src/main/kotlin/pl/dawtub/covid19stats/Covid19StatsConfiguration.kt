2b4b9c81606304089301906c6e8b20d1bd69dc4a
Dawid Tubisz <dawtub002@pbs.edu.pl>
2022-01-29 19:16:41 +0100

    implemented job for fetching statistic data

---
+++ src/main/kotlin/pl/dawtub/covid19stats/Covid19StatsConfiguration.kt
@@ -0,0 +1,8 @@
+package pl.dawtub.covid19stats
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class Covid19StatsConfiguration
